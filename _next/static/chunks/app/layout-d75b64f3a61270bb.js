b304ea330e19700fc9b12f2eee6e7eb73ecc77b5
bdst-141541 <mr.bojangles.141541@outlook.jp>
2025-10-14 10:49:39 +0900

    Delete _next/static directory
    Remove unnecessary Next.js build directory

--- _next/static/chunks/app/layout-d75b64f3a61270bb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2061:function(n,e,u){Promise.resolve().then(u.t.bind(u,54,23))},54:function(){}},function(n){n.O(0,[253,961,744],function(){return n(n.s=2061)}),_N_E=n.O()}]);
